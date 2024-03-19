f930f5eedddc8b467ca28e4ed504e139e816c2c0
Nicolas Bongartz <nicolas.bongartz@student.uhasselt.be>
2024-03-19 18:11:21 +0100

    Add guessing of workgroup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { GPUSorter, SortBuffers } from "./sorter";
-import { upload_to_buffer } from "./utils";
+import { upload_to_buffer, guess_workgroup_size } from "./utils";
 
 async function initWebGPU(): Promise<GPUDevice> {
     if (!navigator.gpu) {
@@ -22,22 +22,24 @@ async function initWebGPU(): Promise<GPUDevice> {
 
 async function main(): Promise<void> {
     let device: GPUDevice = await initWebGPU();
-    let sorter: GPUSorter = new GPUSorter(device, 32);
+    let sorter: GPUSorter = new GPUSorter(device, await guess_workgroup_size(device, device.queue));
 
     let n: number = 10;
     let sort_buffers: SortBuffers = sorter.create_sort_buffers(device, n);
 
-    let keys_scrambled: Float32Array = new Float32Array([...Array(n).keys()].reverse());
+    // let keys_scrambled: Float32Array = new Float32Array([...Array(n).keys()].reverse());
+    let keys_scrambled: Float32Array = new Float32Array(Array.from({length: n}, () => Math.random()));
     let values_scrambled: Float32Array = new Float32Array(Array.from({length: n}, () => Math.floor(Math.random() * n)));
 
     let encoder: GPUCommandEncoder = device.createCommandEncoder();
 
-    upload_to_buffer(
-        encoder,
-        sort_buffers.keys(),
-        device,
-        keys_scrambled
-    );
+    // upload_to_buffer(
+    //     encoder,
+    //     sort_buffers.keys(),
+    //     device,
+    //     keys_scrambled
+    // );
+    device.queue.writeBuffer(sort_buffers.keys(), 0, keys_scrambled, 0, n); // For floats
     upload_to_buffer(
         encoder,
         sort_buffers.values(),
@@ -57,9 +59,11 @@ async function main(): Promise<void> {
 
     await resultBuffer.mapAsync(GPUMapMode.READ);
     const result = new Uint32Array(resultBuffer.getMappedRange(0, sort_buffers.keys_valid_size()));
-
+    
+    console.log("keys", JSON.parse(JSON.stringify(keys_scrambled))); // https://developer.mozilla.org/en-US/docs/Web/API/console/log_static#logging_objects
     console.log("values", values_scrambled);
-    console.log("sorted values (reverse keys)", result);
+    console.log("keys sorted", keys_scrambled.sort());
+    console.log("values sorted", result);
     resultBuffer.unmap();
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -39,60 +39,61 @@ export function upload_to_buffer(encoder: GPUCommandEncoder, buffer: GPUBuffer, 
     encoder.copyBufferToBuffer(staging_buffer, 0, buffer, 0, staging_buffer.size);
 }
 
-// async function test_sort(sorter: GPUSorter, device: GPUDevice, queue: GPUQueue): boolean {
-//     // simply runs a small sort and check if the sorting result is correct
-//     let n = 8192; // means that 2 workgroups are needed for sorting
-//     let scrambled_data: Vec<f32> = (0..n).rev().map(|x| x as f32).collect();
-//     let sorted_data: Vec<f32> = (0..n).map(|x| x as f32).collect();
+async function test_sort(sorter: GPUSorter, device: GPUDevice, queue: GPUQueue): Promise<boolean> {
+    // simply runs a small sort and check if the sorting result is correct
+    let n = 8192; // means that 2 workgroups are needed for sorting
+    let scrambled_data: Uint32Array = new Uint32Array([...Array(n).keys()].reverse());
+    let sorted_data: Uint32Array = new Uint32Array([...Array(n).keys()]);
 
-//     let sort_buffers = sorter.create_sort_buffers(device, NonZeroU32::new(n).unwrap());
+    let sort_buffers = sorter.create_sort_buffers(device, n);
 
-//     let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-//         label: Some("GPURSSorter test_sort"),
-//     });
-//     upload_to_buffer(
-//         &mut encoder,
-//         &sort_buffers.keys(),
-//         device,
-//         scrambled_data.as_slice(),
-//     );
+    let encoder: GPUCommandEncoder = device.createCommandEncoder({
+        label: "GPURSSorter test_sort",
+    });
+    upload_to_buffer(
+        encoder,
+        sort_buffers.keys(),
+        device,
+        scrambled_data
+    );
+    sorter.sort(encoder, queue, sort_buffers);
+    
+    const resultBuffer = device.createBuffer({
+        label: "result buffer",
+        size: sort_buffers.keys_valid_size(),
+        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+    });
+    encoder.copyBufferToBuffer(sort_buffers.keys(), 0, resultBuffer, 0, resultBuffer.size);
+    queue.submit([encoder.finish()]);
+    
+    await resultBuffer.mapAsync(GPUMapMode.READ);
+    const result = new Uint32Array(resultBuffer.getMappedRange(0, sort_buffers.keys_valid_size()));
+    let same: boolean = JSON.stringify(result) == JSON.stringify(sorted_data);
+    resultBuffer.unmap();
 
-//     sorter.sort(&mut encoder, queue, &sort_buffers,None);
-//     let idx = queue.submit([encoder.finish()]);
-//     device.poll(wgpu::Maintain::WaitForSubmissionIndex(idx));
+    return same;
+}
 
-//     let sorted = download_buffer::<f32>(
-//         &sort_buffers.keys(),
-//         device,
-//         queue,
-//         0..sort_buffers.keys_valid_size(),
-//     )
-//     .await;
-//     return sorted.into_iter().zip(sorted_data.into_iter()).all(|(a,b)|a==b);
-// }
+// Function guesses the best subgroup size by testing the sorter with
+// subgroup sizes 1,8,16,32,64,128 and returning the largest subgroup size that worked.
+export async function guess_workgroup_size(device: GPUDevice, queue: GPUQueue): Promise<number> {
+    let cur_sorter: GPUSorter = null;
 
-/// Function guesses the best subgroup size by testing the sorter with
-/// subgroup sizes 1,8,16,32,64,128 and returning the largest subgroup size that worked.
-// async function guess_workgroup_size(device: GPUDevice, queue: GPUQueue): number {
-//     let cur_sorter: GPUSorter = null;
+    // console.log("Searching for the maximum subgroup size (wgpu currently does not allow to query subgroup sizes)");
 
-//     console.log("Searching for the maximum subgroup size (wgpu currently does not allow to query subgroup sizes)");
+    let best: number = null;
+    let elements: Array<number> = [1, 8, 16, 32, 64, 128];
+    for (let i = 0; i < elements.length; ++i) {
+        let subgroup_size = elements[i];
+        // console.log("Checking sorting with subgroupsize {}", subgroup_size);
 
-//     let best = null;
-//     [1, 8, 16, 32, 64, 128].every((subgroup_size) => {
-//         console.log("Checking sorting with subgroupsize {}", subgroup_size);
+        cur_sorter = new GPUSorter(device, subgroup_size);
+        let sort_success = await test_sort(cur_sorter, device, queue);
 
-//         cur_sorter = new GPUSorter(device, subgroup_size);
-//         let sort_success = test_sort(cur_sorter, device, queue).await;
-
-//         console.log("{} worked: {}", subgroup_size, sort_success);
-
-//         if (!sort_success) {
-//             return false;
-//         } else {
-//             best = subgroup_size;
-//             return true;
-//         }
-//     });
-//     return best;
-// }
+        // console.log("{} worked: {}", subgroup_size, sort_success);
+        if (sort_success) {
+            best = subgroup_size;
+        }
+    }
+    return best;
+}
